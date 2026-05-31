b0cb765c39f48601f77499f8988fbdd9e1756f2d
mistrihub <mistrihub75@gmail.com>
2026-05-31 13:49:46 +0530

    Delete start-frontend.cmd

--- start-frontend.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-title TrustLoop Frontend
-cd /d "%~dp0client"
-echo Starting TrustLoop frontend...
-echo.
-npm.cmd run dev
-echo.
-echo Frontend stopped. Press any key to close.
-pause >nul
