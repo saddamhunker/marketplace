1ecbd62613175a5ea70765d283aca5ff0b5c817e
mistrihub <mistrihub75@gmail.com>
2026-05-31 13:49:37 +0530

    Delete start-backend.cmd

--- start-backend.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-title TrustLoop Backend
-cd /d "%~dp0"
-echo Starting TrustLoop backend...
-echo.
-npm.cmd run dev:server
-echo.
-echo Backend stopped. Press any key to close.
-pause >nul
